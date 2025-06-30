429c4796909182f4b5ecc143ec92c1d71f3aa8f7
SIST <SIST@DESKTOP-SF4BBK6>
2025-06-30 11:42:50 +0900

    -feat : "유저 관련 기능 구현"

---
+++ src/main/java/com/sist/web/controller/UserController.java
@@ -0,0 +1,465 @@
+package com.sist.web.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import com.sist.common.model.FileData;
+import com.sist.common.util.StringUtil;
+import com.sist.web.model.Response;
+import com.sist.web.model.User;
+import com.sist.web.service.UserService;
+import com.sist.web.util.CookieUtil;
+import com.sist.web.util.HttpUtil;
+import com.sist.web.util.JsonUtil;
+import com.sist.web.util.SessionUtil;
+
+@Controller("userController")
+public class UserController 
+{
+	private static Logger logger = LoggerFactory.getLogger(UserController.class);
+	
+	
+	@Value("#{env['upload.save.dir']}")
+	private String UPLOAD_SAVE_DIR;
+	
+	@Autowired
+	private	 UserService userService;
+	
+	@Value("#{env['auth.cookie.name']}")
+	private String AUTH_COOKIE_NAME;
+	
+	
+	//로그인 페이지
+	@RequestMapping(value = "/user/login", method=RequestMethod.GET)
+	public String login(HttpServletRequest request, HttpServletResponse response)
+	{
+		return "/user/login";
+	}	
+	
+	//로그인
+	@RequestMapping(value="/user/loginProc", method=RequestMethod.POST)
+	@ResponseBody
+	public Response<Object> loginProc(HttpServletRequest request, 
+											HttpServletResponse response)
+	{
+		Response<Object> ajaxResponse = new Response<Object>();
+		
+		String userId = HttpUtil.get(request, "userId");
+		String userPassword = HttpUtil.get(request, "userPassword");
+		
+		if(!StringUtil.isEmpty(userId) && !StringUtil.isEmpty(userPassword))
+		{
+			User user = userService.userSelect(userId);
+			
+			if(user != null)
+			{
+				if(StringUtil.equals(userPassword, user.getUserPassword()))
+				{
+					
+					if(StringUtil.equals(user.getUserOut(), "N"))
+					{
+						CookieUtil.addCookie(response, "/", -1, AUTH_COOKIE_NAME, 
+                                CookieUtil.stringToHex(userId));
+						
+				        // 로그인 성공 → 세션 저장
+				        HttpSession session = request.getSession(true); // 세션이 없으면 생성
+				        session.setAttribute("userId", user.getUserId());
+				        logger.debug("SessionUserId 111: " + user.getUserId());
+				        logger.debug("SessionUserId 222: " + userId);
+				        
+						logger.debug("userId : " + userId);
+						logger.debug("userPassword hex : " + CookieUtil.stringToHex(userId));
+						ajaxResponse.setResponse(0, "success");
+					}
+					else
+					{
+						ajaxResponse.setResponse(-99, "status error");
+					}
+				}
+				else
+				{
+					//비밀번호 불일치
+					ajaxResponse.setResponse(-1, "password mismatch");
+				}
+			}
+			else
+			{
+				ajaxResponse.setResponse(404, "not found");
+			}
+		}
+		else
+		{
+			ajaxResponse.setResponse(400, "Bad Request");
+		}
+		
+		if(logger.isDebugEnabled())
+		{
+			logger.debug("[UserController]/user/login response \n" +
+										JsonUtil.toJsonPretty(ajaxResponse));
+		}
+
+		return ajaxResponse;	
+	}
+	
+	//로그아웃
+	@RequestMapping(value="/user/loginOut", method=RequestMethod.GET)
+	public String loginOut(HttpServletRequest request, HttpServletResponse response)
+	{
+		
+		// 세션 종료
+	    request.getSession().invalidate();
+	    
+		if(CookieUtil.getCookie(request, AUTH_COOKIE_NAME) != null)
+		{
+			CookieUtil.deleteCookie(request, response, "/", AUTH_COOKIE_NAME);
+		}
+		
+		return "redirect:/user/login";
+	}
+	
+	//회원가입화면
+	@RequestMapping(value="/user/userRegForm", method=RequestMethod.GET)
+	public String userRegForm(HttpServletRequest request, HttpServletResponse response)
+	{
+		String cookieUserId = CookieUtil.getHexValue(request, AUTH_COOKIE_NAME);
+		logger.debug("cookieUserId : " + cookieUserId);
+		
+		if(!StringUtil.isEmpty(cookieUserId))
+		{
+			CookieUtil.deleteCookie(request, response, "/", AUTH_COOKIE_NAME);
+			
+			return "redirect:/user/login";
+			
+		}
+		else
+		{
+			return "/user/userRegForm";
+		}
+	}
+	
+	//아이디 중복체크
+	@RequestMapping(value="/user/idCheck", method=RequestMethod.POST)
+	@ResponseBody
+	public Response<Object> idCheck(HttpServletRequest request, HttpServletResponse response)
+	{
+		Response<Object> ajaxResponse = new Response<Object>();
+		
+		String userId = HttpUtil.get(request, "userId");
+		
+		if(!StringUtil.isEmpty(userId))
+		{
+			if(userService.userSelect(userId) == null)
+			{
+				//사용가능한 아이디
+				ajaxResponse.setResponse(0, "success");
+			}
+			else
+			{
+				//중복아이디 발생
+				ajaxResponse.setResponse(100, "deplicate id");		
+			}	
+		}
+		else
+		{
+			ajaxResponse.setResponse(400, "bad request");
+		}
+		
+		
+		if(logger.isDebugEnabled())
+		{
+			logger.debug("[UserController]/user/idCheck response \n" + 
+									JsonUtil.toJsonPretty(ajaxResponse));
+		}
+
+		return ajaxResponse;
+	}
+	
+	//회원등록
+	@RequestMapping(value="/user/userRegProc", method=RequestMethod.POST)
+	@ResponseBody
+	public Response<Object> userRegProc(MultipartHttpServletRequest request, HttpServletResponse response)
+	{
+		Response<Object> ajaxResponse = new Response<Object>();
+		String userId = HttpUtil.get(request, "userId", "");
+		String userPassword = HttpUtil.get(request, "userPassword2");
+		String userName = HttpUtil.get(request, "userName", "");
+		String userGender = HttpUtil.get(request, "userGender", "");
+		String userNumber = HttpUtil.get(request, "userNumber", "");
+		String zipCode = HttpUtil.get(request, "zipCode","");
+		String streetAdr = HttpUtil.get(request, "streetAdr","");
+		String detailAdr = HttpUtil.get(request, "detailAdr","");
+		String userAdd = zipCode + "|" + streetAdr + "|" +detailAdr;
+		String userBirth = HttpUtil.get(request, "userBirth","");
+		String userEmail = HttpUtil.get(request, "userEmail","");
+		FileData fileData = HttpUtil.getFile(request, "userProfile", UPLOAD_SAVE_DIR, userId);
+		
+		if(!StringUtil.isEmpty(userId) && !StringUtil.isEmpty(userPassword) && !StringUtil.isEmpty(userName) &&
+				!StringUtil.isEmpty(userGender) && !StringUtil.isEmpty(userNumber) && !StringUtil.isEmpty(userAdd) &&
+					!StringUtil.isEmpty(userBirth)&& !StringUtil.isEmpty(userEmail))
+		{
+			//아이디 중복 체크
+			if(userService.userSelect(userId) == null)
+			{
+				User user = new User();
+				user.setUserId(userId);
+				user.setUserPassword(userPassword);
+				user.setUserName(userName);
+				user.setUserGender(userGender);
+				user.setUserNumber(userNumber);
+				user.setUserAdd(userAdd);
+				user.setUserBirth(userBirth);
+				user.setUserEmail(userEmail);
+//				user.setUserProfile(fileData.getFileExt()); // 혹은 저장 후 파일명
+	            if (fileData != null && fileData.getFileName() != null) {
+	                user.setUserProfile(fileData.getFileExt()); // 혹은 저장 후 파일명
+	            }
+				
+				if(userService.userInsert(user) > 0)
+				{
+					ajaxResponse.setResponse(0, "success");
+				}
+				else
+				{
+					ajaxResponse.setResponse(500, "internal server error");
+				}
+
+			}
+			else
+			{
+				//중복 아이디 존재시
+				ajaxResponse.setResponse(100, "duplicate id");
+			}
+		}
+		else
+		{
+			ajaxResponse.setResponse(400, "bad request");
+		}
+		
+		if(logger.isDebugEnabled())
+		{
+			logger.debug("[UserController]/user/regProc response \n" +
+										JsonUtil.toJsonPretty(ajaxResponse));
+		}
+		
+		return ajaxResponse;
+		
+	}
+	
+	//회원정보 수정화면           
+	//디스패쳐 서블릿에서 호출
+	@RequestMapping(value="/user/userUpdateForm", method=RequestMethod.GET)
+	public String userUpdateForm(ModelMap model, HttpServletRequest request, HttpServletResponse response)
+	{
+		//쿠키를 가져옴
+		String cookieUserId = CookieUtil.getHexValue(request, AUTH_COOKIE_NAME);
+		
+		User user = userService.userSelect(cookieUserId);
+		
+		String userAdd = user.getUserAdd(); // 예: "12345 서울시 강남구 테헤란로 1길 5"
+		String zipCode = "";
+		String streetAdr = "";
+		String detailAdr = "";
+		
+		if (userAdd != null) {
+		    String[] parts = userAdd.split("\\|");
+
+		    zipCode = (parts.length > 0) ? parts[0] : "";
+		    streetAdr = (parts.length > 1) ? parts[1] : "";
+		    detailAdr = (parts.length > 2) ? parts[2] : "";
+		}
+		
+		String userProfile = user.getUserProfile();
+		model.addAttribute("zipCode", zipCode);
+		model.addAttribute("streetAdr", streetAdr);
+		model.addAttribute("detailAdr", detailAdr);
+		
+		model.addAttribute("userProfile", userProfile);
+		
+		model.addAttribute("user", user);
+
+		return "/user/userUpdateForm";
+	}
+	
+	//회원정보 수정(ajax 통신용)
+	@RequestMapping(value="/user/userUpdateProc", method=RequestMethod.POST)
+	@ResponseBody
+	public Response<Object> userUpdateProc(MultipartHttpServletRequest request, 
+			HttpServletResponse response)
+	{
+		Response<Object> ajaxResponse = new Response<Object>();
+		
+		String userId = HttpUtil.get(request, "userId", "");
+		String userName = HttpUtil.get(request, "userName", "");
+		String userGender = HttpUtil.get(request, "userGender", "");
+		String userNumber = HttpUtil.get(request, "userNumber", "");
+		String zipCode = HttpUtil.get(request, "zipCode","");
+		String streetAdr = HttpUtil.get(request, "streetAdr","");
+		String detailAdr = HttpUtil.get(request, "detailAdr","");
+		String userAdd = zipCode + "|" + streetAdr + "|" +detailAdr;
+		String userBirth = HttpUtil.get(request, "userBirth","");
+		String userEmail = HttpUtil.get(request, "userEmail","");
+		FileData fileData = HttpUtil.getFile(request, "userProfile", UPLOAD_SAVE_DIR, userId);
+		
+	
+		if (userBirth != null) 
+		{
+			userBirth = userBirth.replaceAll("-", "");
+		}
+		//cookieUserId 와 userid는 같아야 수정가능
+		String cookieUserId = CookieUtil.getHexValue(request, AUTH_COOKIE_NAME);
+		
+		if(!StringUtil.isEmpty(cookieUserId))
+		{
+			if(StringUtil.equals(userId, cookieUserId))
+			{
+				User user = userService.userSelect(cookieUserId);
+				
+				if(user != null)
+				{
+					if(!StringUtil.isEmpty(userId) && !StringUtil.isEmpty(userName) &&
+							!StringUtil.isEmpty(userGender) && !StringUtil.isEmpty(userNumber) && 
+								!StringUtil.isEmpty(userAdd) && !StringUtil.isEmpty(userBirth) && 
+									!StringUtil.isEmpty(userEmail))
+					{
+						user.setUserId(userId);
+						user.setUserName(userName);
+						user.setUserGender(userGender);
+						user.setUserNumber(userNumber);
+						user.setUserAdd(userAdd);
+						user.setUserBirth(userBirth);
+						user.setUserEmail(userEmail);
+						
+						
+						if (fileData != null && fileData.getFileName() != null) {
+			                user.setUserProfile(fileData.getFileExt()); // 혹은 저장 후 파일명
+			            }
+						
+						if(userService.userUpdate(user) > 0)
+						{
+							ajaxResponse.setResponse(0, "success");
+						}
+						else
+						{
+							ajaxResponse.setResponse(500, "internal server error");
+						}
+						
+					}
+					else
+					{
+						//파라미터 값이 올바르지 않을 경우
+						ajaxResponse.setResponse(400, "bad request");
+					}
+					
+				}
+				else
+				{
+					//사용자 정보가 없는경우
+					CookieUtil.deleteCookie(request, response, "/", AUTH_COOKIE_NAME);
+					ajaxResponse.setResponse(404, "not found");
+				}
+				
+			}
+			else
+			{
+				//쿠키정보와 넘어온 userId가 다른 경우
+				CookieUtil.deleteCookie(request, response, "/", AUTH_COOKIE_NAME);
+				ajaxResponse.setResponse(430, "id infomation is different");	
+			}
+		}
+		else
+		{
+			ajaxResponse.setResponse(410, "loging failed");
+		}
+		
+		if(logger.isDebugEnabled())
+		{
+			logger.debug("[UserController]/user/userUpdateProc response \n" +
+					JsonUtil.toJsonPretty(ajaxResponse));
+		}
+		
+		return ajaxResponse;
+	}
+	
+	
+	//회원가입화면
+	@RequestMapping(value="/user/userPasswordForm", method=RequestMethod.GET)
+	public String userPasswordForm(HttpServletRequest request, HttpServletResponse response)
+	{
+		//쿠키값
+		//String cookieUserId = CookieUtil.getHexValue(request, AUTH_COOKIE_NAME);
+		return "/user/userPasswordForm";
+	}
+	
+	//회원 패스워드 변경
+	@RequestMapping(value="/user/userPasswordChange", method=RequestMethod.POST)
+	@ResponseBody
+	public Response<Object> userPasswordChange(HttpServletRequest request, HttpServletResponse response)
+	{
+		Response<Object> ajaxResponse = new Response<Object>();
+		{
+			String cookieUserId = CookieUtil.getHexValue(request, AUTH_COOKIE_NAME);
+			String newPassword = HttpUtil.get(request, "userPassword");
+			
+			logger.debug("입력한 비밀번호3333333:>>>>>>>>>>><<<<<<<<<<<<<<<<"+newPassword);
+			
+			User user = userService.userSelect(cookieUserId);
+			
+			user.setUserPassword(newPassword);
+			
+			if(userService.userPasswordChange(user) > 0)
+			{
+				ajaxResponse.setResponse(0, "success");
+			}
+			else 
+			{
+				ajaxResponse.setResponse(500, "internal server error");
+			}	
+		}
+			
+		if(logger.isDebugEnabled())
+		{
+			logger.debug("[UserController]/user/userPasswordChange response \n" +
+					JsonUtil.toJsonPretty(ajaxResponse));
+		}
+		
+		return ajaxResponse;
+	}
+	
+	//현재비밀번호 체크
+	@RequestMapping(value="/user/checkCurrentPassword")
+	@ResponseBody
+	public boolean checkCurrentPassword(HttpServletRequest request)
+	{
+		String currentPassword = HttpUtil.get(request, "currentPassword");
+		String cookieUserId = CookieUtil.getHexValue(request, AUTH_COOKIE_NAME);
+		
+		logger.debug("입력한 비밀번호:>>>>>>>>>>><<<<<<<<<<<<<<<<"+currentPassword);
+		
+		User user = userService.userSelect(cookieUserId);
+		
+		logger.debug("DB 비밀번호: >>>>>>>>>>><<<<<<<<<<<<<<<<" + user.getUserPassword());
+		return user.getUserPassword().equals(currentPassword);
+
+	}
+	
+	//회원댓글관리
+	@RequestMapping(value="/user/userCommentForm", method=RequestMethod.GET)
+	public String userCommentForm(HttpServletRequest request, HttpServletResponse response)
+	{
+		//쿠키값
+		//String cookieUserId = CookieUtil.getHexValue(request, AUTH_COOKIE_NAME);
+		return "/user/userCommentForm";
+	}
+}
---
+++ src/main/webapp/WEB-INF/views/user/userUpdateForm.jsp
@@ -0,0 +1,369 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ include file="/WEB-INF/views/include/taglib.jsp" %>
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+<%@ include file="/WEB-INF/views/include/head.jsp" %>
+  <title>회원가입수정</title>
+  <style>
+    .signup-container {
+    max-width: 400px;
+    margin: 0 auto;
+    padding: 30px;
+    border: 1px solid #ddd;
+    border-radius: 10px;
+    font-family: 'Arial', sans-serif;
+    background-color: #fff;
+  }
+
+  .signup-container h2 {
+    text-align: center;
+    margin-bottom: 30px;
+  }
+
+  .form-group {
+    margin-bottom: 15px;
+    display: flex;
+    flex-direction: column;
+  }
+
+  label {
+    font-weight: bold;
+    margin-bottom: 5px;
+  }
+
+  input[type="text"],
+  input[type="password"],
+  input[type="email"],
+  input[type="file"],
+  select {
+    padding:10px;
+    font-size: 14px;
+    border: 1px solid #ccc;
+    border-radius: 6px;
+  }
+
+  input[readonly] {
+    background-color: #f5f5f5;
+    cursor: not-allowed;
+    
+  }
+
+  .address-group {
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+  }
+
+  .submit-btn {
+    width: 100%;
+    padding: 12px;
+    font-size: 16px;
+    background-color: #2d7df6;
+    color: white;
+    border: none;
+    border-radius: 6px;
+    cursor: pointer;
+    margin-top: 20px;
+  }
+
+  .submit-btn:hover {
+    background-color: #135fd4;
+  }
+  
+ 
+  </style>
+  <script type="text/javascript">
+  	$(document).ready(function(){
+  		$("#userId").focus();
+  		
+  		$("#btnReg").on("click",function(){
+  			//공백체크
+  			var emptCheck = /\s/g;
+  			
+  			//영문 대소문자, 숫자로만 이루어진 4~12자리 정규식
+  			var idPwCheck = /^[a-zA-Z0-9]{4,12}$/;
+  			
+  			// 010-1234-5678 또는 01012345678 형식 허용
+  			var phoneCheck = /^01[016789]-?\d{3,4}-?\d{4}$/;
+  			
+  			// 한글, 영어, 숫자, 공백, 일부 특수문자(-, .) 허용 / 최소 5자 이상
+  			var addressCheck = /^[가-힣a-zA-Z0-9\s\-\.\,]{5,100}$/;
+  			
+  			if($.trim($("#userId").val()).length <= 0)
+  			{
+  				alert("회원 아이디를 입력하세요.");
+  				$("#userId").val("");
+  				$("#userId").focus();
+  				return;
+  			}
+  			
+  			if(emptCheck.test($("#userId").val()))
+  			{
+  				alert("회원 아이디는 공백을 포함할수 없습니다.");
+  				$("#userId").focus();
+  				return;
+  			}
+  			
+  			if(!idPwCheck.test($("#userId").val()))
+  			{
+  				alert("회원 아이디는 4~12자의 영문 대소문자와 숫자로만 입력가능합니다.");
+  				$("#userId").val("");
+  				$("#userId").focus();
+  				return;
+  			}
+  		
+  			
+  			if($("#userGender").val() == "")
+  			{
+  				alert("성별을 선택하세요");
+  				$("#userGender").focus();
+  				return;
+  			}
+  			
+  			var userPhone = $.trim($("#userNumber").val());
+  			if (!phoneCheck.test(userPhone)) {
+  			  alert("전화번호는 010-1234-5678 형식으로 입력하세요.");
+  			  $("#userNumber").focus();
+  			  return;
+  			}
+  			
+  			//var userAdd = $.trim($("#userAdd").val());
+  			
+  			//if (!addressCheck.test(userAdd)) 
+  			//{
+  			//  alert("주소는 한글/영문/숫자 조합으로 5자 이상 입력해야 합니다.");
+  			//  $("#userAdd").focus();
+  			//  return;
+  			//}
+  			
+  			
+  			var birth = $.trim($("#userBirth").val());
+  			var birthCheck = /^\d{8}$/;
+
+  			if (!birthCheck.test(birth)) 
+  			{
+  			  alert("생년월일은 YYYYMMDD 형식의 8자리 숫자로 입력하세요.");
+  			  $("#userBirth").focus();
+  			  return;
+  			}
+  			
+  			if($.trim($("#userEmail").val()) <= 0)
+  			{
+  				alert("회원 이메일을 입력하세요.");
+  				$("#userEmail").val("");
+  				$("#userEmail").focus();
+  				return;
+  			}
+  			
+  			if(!fn_validateEmail($("#userEmail").val()))
+  			{
+  				alert("회원 이메일 형식이 올바르지 않습니다.");
+  				$("#userEmail").focus();
+  				return;
+  			}
+  			
+  		});	
+  		
+  		$("#btnUpdate").on("click", function(){
+  			var form = $("#updateForm")[0];
+  			var formData = new FormData(form);		//자바스크립트에서 폼 데이터를 다루는 객체
+			
+  			$.ajax({
+  				type:"POST",
+  				enctype:"multipart/form-data",
+  				url:"/user/userUpdateProc",
+  				data:formData,
+  				processData:false,
+  				contentType:false,
+  				cache:false,
+  				beforeSend:function(xhr)
+  				{
+  					xhr.setRequestHeader("AJAX", "true");
+  				},
+  				success:function(response)
+  	  			{
+  	  				if(response.code == 0)
+  	  				{
+  	  					alert("회원정보가 수정되었습니다.");
+  	  					location.href = "/user/userUpdateForm";
+  	  				}
+  	  				else if(response.code == 400)
+  	  				{
+  	  					alert("파라미터 값이 올바르지 않습니다.");
+  	  					$("#userId").focus();
+  	  				}
+  	  				else if(response.code == 404)
+  	  				{
+  	  					alert("회원정보가 존재하지 않습니다.");
+  						location.href = "/user/login";
+  	  				}
+  	  				else if(response.code == 410)
+  	  				{
+  	  					alert("로그인을 먼저 하세요.");
+  						location.href = "/user/login";
+  	  				}
+  	  				else if(response.code == 430)
+  	  				{
+  	  					alert("아이디 정보가 다릅니다.");
+  						location.href = "/user/login";
+  	  				}
+  	  				else if(response.code == 500)
+  	  				{
+  	  					alert("회원정보 수정 중 오류가 발생하였습니다.");
+  						$("#userId").focus();
+  	  				}
+  	  				else
+  	  				{
+  	  					alert("회원정보 수정 중 오류 발생");
+  						$("#userId").focus();
+  	  				}
+  	  			},
+  	  			error:function(xhr, status, error)
+	  			{
+	  				icia.common.error(error);
+	  			}
+  			});
+  		}); 		
+  	});
+  	
+  	function fn_validateEmail(value)
+  	{
+  		var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+  		
+  		return emailReg.test(value);
+  	}
+  	
+  	 function execPostCode()
+  	 {
+       new daum.Postcode({
+           oncomplete: function(data) {
+           	// 우편번호
+               $("#zipCode").val(data.zonecode);
+               // 도로명 및 지번주소
+               $("#streetAdr").val(data.roadAddress);
+           }
+       }).open();
+  	 } 
+  	    
+	function addrCheck() 
+	{
+        if($("#zipCode").val() == '' && $("#streetAdr").val() == ''){
+            alert("우편번호를 클릭하여 주소를 검색해주세요.");
+            $("#zipCode").focus();
+        }
+    }
+	
+	function formatBirthDate(el) {
+	   var value = el.value; // 예: 2025-06-24
+	    if (value) {
+	      var formatted = value.replace(/-/g, ''); // 20250624
+	      el.type = "text"; // type을 text로 바꾸면 변경된 값 표시 가능
+	      el.value = formatted;
+	    }
+	  }
+
+  	</script>
+  	 <!-- 우편번호 daum api -->
+  	 <script src="//t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js"></script>
+  	 
+  	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
+  	<script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
+  	
+  	
+</head>
+<body>
+<%@ include file="/WEB-INF/views/include/navigation.jsp" %>
+  <div class="signup-container">
+    <h2>회원가입수정</h2>
+
+    <form id="updateForm" name="updateForm" method="post" enctype="multipart/form-data">
+      <div class="form-group">
+	  <c:if test="${!empty user.userProfile}">
+	    <!-- 이미지가 있을 때만 label + 이미지 표시 -->
+	    <div style="display: flex; align-items: center; gap: 10px;">
+	      <label style="margin: 0;">프로필 사진</label>
+	      <img src="/resources/upload/${user.userId}.${userProfile}"
+	           style="width: 80px; height: 80px; object-fit: cover; border-radius: 5px; border: 1px solid #ccc;" />
+	    </div>
+	  </c:if>
+	  	프로필 파일변경<input type="file" id="userProfile" name="userProfile">
+        <c:if test="${!empty user.userProfile}">
+		    <div style="margin-bottom:0.3em;">[첨부파일 : ${user.userId}.${user.userProfile}]</div>
+        </c:if>
+	</div>
+	
+      <div class="form-group">
+        <label>아이디</label>
+        <input type="text" id="userId" name="userId" value="${user.userId}" maxlength="20">
+      </div>
+
+      <div class="form-group">
+        <label>이름</label>
+        <input type="text" id="userName" name="userName" value="${user.userName}"  readonly maxlength="20">
+      </div>
+
+      <div class="form-group">
+        <label>성별</label>
+        <select id="userGender" name="userGender" value="${user.userGender}">
+          <option value="">선택</option>
+          <option value="M" <c:if test="${user.userGender == 'M'}">selected</c:if>>남자</option>
+          <option value="F" <c:if test="${user.userGender == 'F'}">selected</c:if>>여자</option>
+        </select>
+      </div>
+      
+      <div class="form-group">
+        <label>전화번호</label>
+        <input type="text" id="userNumber" name="userNumber" value="${user.userNumber}" placeholder="예: 010-1234-5678">
+      </div>
+        
+    <div class="form-group address-group">
+      <label>우편번호</label>
+       <div style="display: flex; align-items: center; gap: 6px; margin-top: 4px;">
+      	 <input type="text" id="zipCode" name="zipCode" placeholder="우편번호" value="${zipCode}" readonly onclick="execPostCode()" style="width: 120px;">
+     	 <button type="button" onclick="execPostCode()"
+            style="height: 45px; padding: 0 20px;">검색</button>
+       </div>
+      <label>도로명 주소</label>
+      <input type="text" id="streetAdr" name="streetAdr" placeholder="도로명 주소" value="${streetAdr}" readonly>
+
+      <label>상세 주소</label>
+      <input type="text" id="detailAdr" name="detailAdr" placeholder="상세 주소" value="${detailAdr}" onclick="addrCheck()">
+    </div>
+      
+      <!-- <div class="form-group">//-->
+      <div class="form-group">
+      <label>생년월일</label>
+      <!--<input type="date" id="userBirth" name="userBirth"  value="${user.userBirth}" placeholder="YYYYMMDD" onchange="formatBirthDate(this)">//-->
+      <c:choose>
+    	<c:when test="${not empty user.userBirth}">
+      		<input type="date" id="userBirth" name="userBirth"
+             	value="${user.userBirth.substring(0,4)}-${user.userBirth.substring(4,6)}-${user.userBirth.substring(6,8)}"
+             	onchange="formatBirthDate(this)" maxlength="8">
+    	</c:when>
+      <c:otherwise>
+      	<input type="date" id="userBirth" name="userBirth" onchange="formatBirthDate(this)"> 	
+     </c:otherwise>
+  </c:choose>
+    </div>
+    
+     <div class="form-group">
+      <label>이메일</label>
+      <input type="email" id="userEmail" name="userEmail" value="${user.userEmail}" maxlength="25">
+     </div>
+    
+      <!--<div class="form-group">
+        <label>프로필 사진</label>
+        <input type="file" id="userProfile" name="userProfile">
+        <c:if test="${!empty user.userProfile}">
+		    <div style="margin-bottom:0.3em;">[첨부파일 : ${user.userId}.${user.userProfile}]</div>
+        </c:if>
+      </div>//-->
+
+      <button type="button" id="btnUpdate" class="submit-btn">수정하기</button>
+    </form>
+  </div>
+</body>
+</html>
+    
