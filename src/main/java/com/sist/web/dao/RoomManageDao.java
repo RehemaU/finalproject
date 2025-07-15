7fa021386510d6c8a729960806bacfaa5fa233f3
SIST <SIST@DESKTOP-KLEFEV3>
2025-07-15 09:04:01 +0900

    사진만 빼고 객실인서트

--- src/main/java/com/sist/web/dao/RoomManageDao.java
+++ src/main/java/com/sist/web/dao/RoomManageDao.java
@@ -5,10 +5,9 @@ import java.util.List;
 import com.sist.web.model.AccommodationRoom;
 
 public interface RoomManageDao {
-    void insertRoom(AccommodationRoom room);
+    void insertAccommodationRoom(AccommodationRoom room);  // 매퍼의 실제 메서드명과 일치
     List<AccommodationRoom> findByAccommId(String accommId);
     AccommodationRoom findByRoomId(String roomId);
     String getNextAccommRoomSeq();
-    List<AccommodationRoom> findRoomsByAccomm(String accommId);  // ✅ 이름 맞추기
 
 }
